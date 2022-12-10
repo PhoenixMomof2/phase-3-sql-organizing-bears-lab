86049b91049f017a40a0887462233730e36ce3ef
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-12-09 20:57:21 -0500

    assignment complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 2, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'M', 'black', 'calm', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 1, 'F', 'brown', 'mean', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 3, 'M', 'black', 'mean', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 3, 'F', 'brown', 'calm', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 2, 'F', 'black', 'calm', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 4, 'F', 'brown', 'mean', 0);
+INSERT INTO bears VALUES (8, NULL, 5, 'M', 'black', 'calm', 1);
